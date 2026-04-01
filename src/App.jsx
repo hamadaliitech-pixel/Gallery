3bc4bd85991e69eb3524cc3287971da1a2f62f23
user.name :"hammad <hamadalii.tech@gmail.com>
2026-04-01 19:39:11 +0500

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Firstpage from "./Gallerycomponents/Firstpage/Firstpage.jsx";
 import Imagecloseup from "./Gallerycomponents/Imagecloseup/Imagecloseup.jsx";
 import Error from "./assets/Error.jsx";
 import Imagedetails from "./Gallerycomponents/Imagecloseup/Imagedetails.jsx";
-import Api, { ApiFactchedData } from "./Api/Api.jsx";
 import { Provider } from "react-redux";
 import { store } from "./ReduxStore/Store.jsx";
 
-
 const App = () => {
-function Mainlayout() {
-  return(
-    <>
-    <Outlet/>
-    </>
-  )
-}
   return (
     <>
       <Provider store={store}>
         <Routes>
-          <Route>
-            <Route path="/" element={<Firstpage />} />
-            {/* <Route path="/Imagecloseup/:id" element={<Imagedetails />} /> */}
-            <Route path="*" element={<Error />} />
-            <Route element={<Mainlayout/>}>
-            <Route path="/Imagecloseup" element={<Imagecloseup />} />
-              {/* <Route path="/Imagecloseup" ele/> */}
-            </Route>
-          </Route>
+          <Route path="/" element={<Firstpage />} />
+          <Route path="/Imagecloseup/:id" element={<Imagedetails />} />
+          <Route path="/Imagecloseup" element={<Imagecloseup />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Provider>
     </>
--- src/Gallerycomponents/Firstpage/Herosection.jsx
+++ src/Gallerycomponents/Firstpage/Herosection.jsx
@@ -1,50 +1,44 @@
-import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { ZoomIn, ArrowRight, ArrowLeft } from "lucide-react";
-import { useNavigate, Route, data } from "react-router-dom";
-import Api, { ApiFactchedData } from "../../Api/Api";
-
-// import Pagechange from "../../Pagechange/Pagechange";
+import { ZoomIn } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { ApiFactchedData } from "../../Api/Api";
 
 const Herosection = () => {
   const navigate = useNavigate();
   const [Render, setRender] = useState([]);
-  const [load, setload] = useState("loading...");
+  const [isLoading, setIsLoading] = useState(true);
 
   const data = useContext(ApiFactchedData);
 
-  async function run() {
-    let date = await data;
-
-    setload([]);
-    setRender(date);
-    return date;
-  }
-  run();
+  useEffect(() => {
+    async function run() {
+      let fetchedData = await data;
+      setRender(fetchedData);
+      setIsLoading(false);
+    }
+    run();
+  }, [data]);
 
 
   return (
     <>
       <div className="w-full flex justify-between items-center text-3xl">
-        {load}
+        {isLoading && "loading..."}
       </div>
 
-      {/* Main_image_container */}
-      <div className="Main_image_container grid grid-cols-2 sm:grid-cols-3 overflow-y-scroll  gap-3 p-2 items-center">
+      <div className="Main_image_container grid grid-cols-2 sm:grid-cols-3 overflow-y-scroll gap-3 p-2 items-center">
         {Render.map((item) => (
           <div
             key={item.id}
             style={{ backgroundImage: `url(${item.download_url})` }}
             className="flex items-end h-60 bg-cover rounded-2xl overflow-hidden sm:h-56 cursor-pointer shadow-black shadow-md hover:scale-95 transition-transform"
             onClick={() => {
-              navigate("./Imagecloseup", {
-                state: {
-                  item: item,
-                },
+              navigate(`/Imagecloseup`, {
+                state: { item: item },
               });
             }}
           >
-            <div className="text-white inset-0 bg-linear-to-t p-2 font-serif from-black/80 to-transparent w-full ">
+            <div className="text-white inset-0 bg-linear-to-t p-2 font-serif from-black/80 to-transparent w-full">
               {item.author}
               <div
                 style={{ fontFamily: "Poppins, sans-serif" }}
@@ -58,13 +52,6 @@ const Herosection = () => {
             </div>
           </div>
         ))}
-        <button
-          onClick={() => {
-            navigate(+1);
-          }}
-        >
-          next
-        </button>
       </div>
     </>
   );
